c460bb9cc68310301abf76424c9850f889886350
Volodymyr Nerovnia <nerv@i.ua>
2022-09-20 23:48:36 +0300

    Create classes:   Message   MessageVisual   Category   MessageHTMLForm and add logic

--- js/main.js
+++ js/main.js
@@ -1,43 +1,233 @@
 class Message {
-  constructor(icon, name, created, cathegory, content, dates, status = "active") {
-    this._icon = icon;
-    this._name = name;
-    this._created = created;
-    this._cathegory = cathegory;
-    this._content = content;
-    this._dates = dates;
-    this._status = status;  // active or archived
-
-    refreshStatistics("add", cathegory);
+  constructor(name, created, cathegory, content, dates) {
+    this.#uuid = crypto.randomUUID();
+    this.#name = name;
+    this.#created = created;
+    this.#cathegory = cathegory;
+    this.#content = content;
+    this.#dates = dates;
   }
 
-  set icon(icon) { this._icon = icon; }
-  get icon() { return this._icon; }
+  get uuid() { return this.#uuid; }
 
-  set name(name) { this._name = name; }
-  get name() { return this._name; }
+  set name(name) { this.#name = name; }
+  get name() { return this.#name; }
 
   set cathegory(cathegory) { 
-    changeCathegory(this._cathegory, cathegory, this._status);
+    changeCathegory(this.#cathegory, cathegory, this.#status);
     //refreshStatistics("update", cathegory);
-    this._cathegory = cathegory; 
+    this.#cathegory = cathegory; 
   }
-  get cathegory() { return this._cathegory; }
+  get cathegory() { return this.#cathegory; }
 
-  set content(content) { this._content = content; }
-  get content() { return this._content; }
+  set content(content) { this.#content = content; }
+  get content() { return this.#content; }
 
-  set dates(dates) { this._dates = dates; }
-  get dates() { return this._dates; }
+  set dates(dates) { this.#dates = dates; }
+  get dates() { return this.#dates; }
+}
+
+class MessageVisual extends Message {
+  constructor(name, created, cathegory, content, dates) {
+    super(name, created, cathegory, content, dates);
+    this.#status = "active";  // active or archived
+  }
+
+  get rowHTMLTable() {
+    return `
+      <tr>
+        <td><img class="tbl-icon" src="${this.#cathegory.icon}" alt="Category"/></td>
+        <td>${this.#name}</td>
+        <td class="text-center">${record.created.toLocaleDateString()}</td>
+        <td>${this.#category.name}</td>
+        <td>${this.#content}</td>
+        <td class="text-center">${this.#dates}</td>
+        <td class="text-center"><button value="${this.#uuid}" name="note-edit"><img class="tbl-icon" src="../images/edit.png" alt="Edit"/></button></td>
+        <td class="text-center"><button value="${this.#uuid}" name="note-arch"><img class="tbl-icon" src="../images/archive.png" alt="Archive"/><button></td>
+        <td class="text-center"><button value="${this.#uuid}" name="note-delete"><img class="tbl-icon" src="../images/delete.png" alt="Delete"/><button></td>
+      </tr>`;
+  }
+
+  set status(status) {
+    this.#status = status; 
+  }
+
+  get status() { return this.#status; }
+
+  get icon() { return this.#cathegory.icon; }
+
 }
 
 class Category {
   constructor(icon, name) {
-    this.icon = icon;
-    this.name = name;
+    this.#icon = icon;
+    this.#name = name;
+  }
+
+  get icon() { return this.#icon; }
+
+  get name() { return this.#name; }
+}
+
+class MessageHTMLForm {
+  /*
+  #form;
+  #inpMessageName;
+  #inpMessageCategory;
+  #inpMessageContent;
+  #errElement;*/
+  constructor(form, inpMessageName, inpMessageCategory, inpMessageContent, inpMessageDates, errElement, mapCategories) {
+    this.#form = form;
+    this.#inpMessageName = inpMessageName;
+    this.#inpMessageCategory = inpMessageCategory;
+    this.#inpMessageContent = inpMessageContent;
+    this.#errElement = errElement;
+
+    this.#form.addEventListener('submit', this.addSubmitEventListener);
+    this.#inpMessageName.addEventListener('blur', this.addBlurEventListener);
+    this.#inpMessageCategory.addEventListener('blur', this.addBlurEventListener);
+    this.#inpMessageContent.addEventListener('blur', this.addBlurEventListener);
+  }
+
+  get form() { return this.#form; }
+  get inpMessageName() { return this.#inpMessageName; }
+  get inpMessageCategory() { return this.#inpMessageCategory; }
+  get inpMessageContent() { return this.#inpMessageContent; }
+  
+  get #allElements() { return [this.#inpMessageName, this.#inpMessageCategory, this.#inpMessageContent];}
+
+  #checkName() {
+    return this.#inpMessageName.value !== "";
+  }
+
+  #checkCategory() {
+    return ((this.#inpCategory.options.length > 0)&&(this.#inpCategory.selectedIndex !== 0));
+    //return this.#inpCategory.options[this.#inpCategory.selectedIndex].text !== "";
+  }
+
+  #checkContent() {
+    return this.#inpMessageName.value !== "";
+  }
+
+  #printError(err) {
+    this.#errElement.textContent = err;
+  }
+
+  isValid() {
+    let valid = true;
+    if (!this.#checkName()) {
+      valid = false;
+      MessageHTMLForm.#setInputError(this.#inpMessageName)
+    } else {
+      MessageHTMLForm.#unsetInputError(this.#inpMessageName)
+    }
+   
+    if (!this.#checkCategory()) {
+      valid = false;
+      MessageHTMLForm.#setInputError(this.#inpMessageCategory)
+    } else {
+      MessageHTMLForm.#unsetInputError(this.#inpMessageCategory)
+    }
+
+    if (!this.#checkContent()) {
+      valid = false;
+      MessageHTMLForm.#setInputError(this.#inpMessageContent)
+    } else {
+      MessageHTMLForm.#unsetInputError(this.#inpMessageContent)
+    }
+    return valid; 
+  }
+
+  static #validateIsEmpty(element) {
+    if (element.value === "") {
+      MessageHTMLForm.#setInputError(element);
+      return true;
+    } 
+    MessageHTMLForm.#unsetInputError(element);
+    return false;
+  }
+
+  addSubmitEventListener(event) {
+    let messageForm = globalThis.MessageHTMLForms.filter(messageHTMLForm => (messageHTMLForm.form.id === event.currentTarget.id));
+    if (!messageForm.isEmpty) {
+      if (messageForm[0].isValid()) {
+        alert('Data in form is valid!')
+      }
+    }
+  }
+
+  addBlurEventListener(event) {
+    MessageHTMLForm.#validateIsEmpty(event.currentTarget)
+  }
+
+  static #setInputError = (element) => {
+    element.classList.remove("input-success");
+    element.classList.add("input-error");
+  }
+  
+  static #unsetInputError = (element) => {
+    element.classList.add("input-success");
+    element.classList.remove("input-error");
+  }
+  
+}
+
+const mapCategories = new Map();
+mapCategories.set("task", new Category("../images/task.png", "Task"));
+mapCategories.set("randth", new Category("../images/random_thought.png", "Random Thought"));
+mapCategories.set("idea", new Category("../images/idea.png", "Idea"));
+mapCategories.set("quote", new Category("../images/quote.png", "Quote"));
+
+const selCategory = document.querySelector("#note-category__create");
+selCategory.insertAdjacentHTML("beforeend", ` <option value="empty">-- Select category --</option>`);
+mapCategories.forEach((category, key) => {
+  console.log(category, key);
+  selCategory.insertAdjacentHTML("beforeend", ` <option value="${key}">${category.name}</option>`);
+});
+
+//const frmCreateMessage = new MessageHTMLForm(
+globalThis.MessageHTMLForms = [];
+globalThis.MessageHTMLForms.push(new MessageHTMLForm(
+  document.querySelector("#frm-note-create"), 
+  document.querySelector("#note-name__create"), 
+  document.querySelector("#note-category__create"), 
+  document.querySelector("#note-content__create"), 
+  document.querySelector("#note-dates__create"), 
+  document.querySelector("#note-dates__create"), 
+  document.querySelector("#note-err__create")
+));
+
+const statistics = {
+  activeNotes: [],
+  archivesNotes: [],
+
+  createNote: function(note_data) {
+    const note = new MessageVisual(note_data.name, note_data.created, note_data.cathegory, note_data.content, note_data.dates);
+    this.activeNotes.push(note);
+    this.refresh(note.cathegory, note.status);
+  },
+
+  deleteNote: function(uuid) {
+    const node = "";
+    //const cathegory = 
+  },
+
+  archiveNote: function(note) {
+
+  },
+
+  activateNote: function(note) {
+
+  },
+
+  refresh(cathegory, status) {
+    
   }
 }
 
+
+
+/*
 // action: add, remove, update
 const refreshStatistics = (action, cathegory, changedField = "") => {
   switch (action) {
@@ -49,10 +239,12 @@ const refreshStatistics = (action, cathegory, changedField = "") => {
       break;
   }
 };
+*/
+//const activeMessages = new Map();
+//const archiveMessages = new Map();
 
-const activeMessages = new Map();
-const archiveMessages = new Map();
 
+/*
 const setInputError = (element) => {
   element.classList.remove("input-success");
   element.classList.add("input-error");
@@ -73,18 +265,15 @@ const inputsToMap = (map) => {
   inp = document.querySelector("#note-dates");
   map.set(inp.id, inp);
 };
+*/
 
-const categories = new Map();
-categories.set("task", new Category("../images/task.png", "Task"));
-categories.set("randth", new Category("../images/random_thought.png", "Random Thought"));
-categories.set("idea", new Category("../images/idea.png", "Idea"));
-categories.set("quote", new Category("../images/quote.png", "Quote"));
-
+/*
 
 const frmInputs = new Map();
 inputsToMap(frmInputs)
 
 frmInputs.forEach(item => {
+  //console.log(item);
   item.addEventListener('blur', event => {
     if (event.currentTarget.value === "") {
       setInputError(event.currentTarget);
@@ -94,16 +283,13 @@ frmInputs.forEach(item => {
   });
 });
 
-const selCategory = frmInputs.get("note-category");
-selCategory.insertAdjacentHTML("beforeend", ` <option value="empty">-- Select category --</option>`);
-categories.forEach((category, key) => {
-  selCategory.insertAdjacentHTML("beforeend", ` <option value="${key}">${category.name}</option>`);
-});
-
+*/
+/*
 const addRecordToMap = record => {
 
 };
-
+*/
+/*
 const addRecordToTable = record => {
   const row = `
     <tr>
@@ -113,21 +299,23 @@ const addRecordToTable = record => {
       <td>${record.category}</td>
       <td>${record.content}</td>
       <td class="text-center">${record.dates}</td>
-      <td class="text-center"><img class="tbl-icon" src="../images/edit.png" alt="Edit"/></td>
-      <td class="text-center"><img class="tbl-icon" src="../images/archive.png" alt="Archive"/></td>
-      <td class="text-center"><img class="tbl-icon" src="../images/delete.png" alt="Delete"/></td>
+      <td class="text-center"><button><img class="tbl-icon" src="../images/edit.png" alt="Edit"/></button></td>
+      <td class="text-center"><button><img class="tbl-icon" src="../images/archive.png" alt="Archive"/><button></td>
+      <td class="text-center"><button><img class="tbl-icon" src="../images/delete.png" alt="Delete"/><button></td>
     </tr>`;
   console.log(categories.get(record.category));
   //console.log(record.category_name);
   document.querySelector("#active-records tbody").insertAdjacentHTML("afterbegin", row);
 };
-
+*/
+/*
 const addNoteRecord = record => {
   addRecordToMap(record);
   addRecordToTable(record);
 };
-
-document.querySelector("#frm-note").addEventListener("submit", event => {
+*/
+/*
+document.querySelector("#frm-note-create").addEventListener("submit", event => {
   event.preventDefault();
   let inputsIsFull = true;
   for (const input of frmInputs.entries()) {
@@ -143,7 +331,31 @@ document.querySelector("#frm-note").addEventListener("submit", event => {
       content: frmInputs.get("note-content").value,
       dates: frmInputs.get("note-dates").value
     });
+  event.currentTarget.reset();
+});*/
+
+
+
+
+/*
+document.querySelector("#frm-note-create").addEventListener("submit", event => {
+  event.preventDefault();
+  let inputsIsFull = true;
+  for (const input of frmInputs.entries()) {
+    if ((input[1].value === "") || (input[1].value === "empty")) inputsIsFull = false;
+  }
+  if (inputsIsFull)
+    statistics.createNote({
+      name: frmInputs.get("note-name").value,
+      created: new Date(),
+      categoryKey: frmInputs.get("note-category").value,
+      category: frmInputs.get("note-category").options[frmInputs.get("note-category").selectedIndex].text,
+      content: frmInputs.get("note-content").value,
+      dates: frmInputs.get("note-dates").value
+    });
+  event.currentTarget.reset();
 });
+*/
 /*
 All form fields must be filled in!
 */
